47b068b61f25c642a3a7e4c88ca2e3cde4b3e4f0
Chirag Prajapati <chiragprajapati6100@gmail.com>
2026-03-10 14:56:26 +0530

    completed exe 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Persons } from './components/Persons'
 import { Filter } from './components/Filter'
 import { PersonForm } from './components/PersonForm'
+import axios from 'axios'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+        .get("http://localhost:3001/persons")
+        .then(response => setPersons(response.data))
+  }, [])
+  
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -26,7 +30,6 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target);
 
     persons.find((person) => person.name === newName)
       ? alert(`${newName} is already added to phonebook `)
